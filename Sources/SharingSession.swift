68326750476b8f65f758af14f8217cab519e69d9
Vytis Šibonis <vytis@wire.com>
2017-08-16 15:50:16 +0200

    Changed group name

--- Sources/SharingSession.swift
+++ Sources/SharingSession.swift
@@ -240,7 +240,7 @@ public class SharingSession {
         
         let environment = ZMBackendEnvironment(userDefaults: UserDefaults.shared())
         let cookieStorage = ZMPersistentCookieStorage(forServerName: environment.backendURL.host!, userIdentifier: accountIdentifier)
-        let reachabilityGroup = ZMSDispatchGroup(dispatchGroup: DispatchGroup(), label: "Session manager reachability")!
+        let reachabilityGroup = ZMSDispatchGroup(dispatchGroup: DispatchGroup(), label: "Sharing session reachability")!
         let serverNames = [environment.backendURL, environment.backendWSURL].flatMap{ $0.host }
         let reachability = ZMReachability(serverNames: serverNames, observer: nil, queue: .main, group: reachabilityGroup)
         
