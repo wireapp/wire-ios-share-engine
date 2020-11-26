a9a6ecfb0af9e01e3a2d952e503a4eaf33d0f999
KaterinaWire <katerina@wire.com>
2020-11-26 23:29:20 +0100

    Clean up

--- Sources/SharingSession.swift
+++ Sources/SharingSession.swift
@@ -199,7 +199,7 @@ public class SharingSession {
 
     private let strategyFactory: StrategyFactory
     
-    public var appLockController: AppLockController
+    public let appLockController: AppLockController
         
     /// Initializes a new `SessionDirectory` to be used in an extension environment
     /// - parameter databaseDirectory: The `NSURL` of the shared group container
